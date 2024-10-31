c7d6c16f026eff0ed2759970889d6bde19020e08
Muhammad Shakir <mshakir7809797@gmail.com>
2024-10-31 12:17:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Home from './PortfolioWebsite/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Nav from './PortfolioWebsite/Nav';
-import About from './PortfolioWebsite/About';
-import Portfolio from './PortfolioWebsite/Portfolio';
-import Contact from './PortfolioWebsite/Contact';
-import Blog from './PortfolioWebsite/Blog';
-import "./PortfolioWebsite/Home.css"
+// import Home from './PortfolioWebsite/Home';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Nav from './PortfolioWebsite/Nav';
+// import About from './PortfolioWebsite/About';
+// import Portfolio from './PortfolioWebsite/Portfolio';
+// import Contact from './PortfolioWebsite/Contact';
+// import Blog from './PortfolioWebsite/Blog';
+// import "./PortfolioWebsite/Home.css"
 
 export default function App() {
   return (
     <>
-    <Router>
+    {/* <Router>
       <div>
         <Nav />
         <Routes>
@@ -22,7 +22,7 @@ export default function App() {
           <Route path="/blog" element={<Blog/>} />
         </Routes>
       </div>
-    </Router>
+    </Router> */}
 
     
     </>
